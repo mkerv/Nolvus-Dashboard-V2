19c8c7852e3da91dbb70467be94f92765e50f1e0
Ishikudeska <masonmesser23@gmail.com>
2024-01-22 10:02:12 -0600

    modified: ApiManager.cs and added a unusable funct

--- API manager/ApiManager.cs
+++ API manager/ApiManager.cs
@@ -1,13 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using Vcc.Nolvus.NexusApi.Responses;
+using System;
+using System.Net.WebSockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
-using Vcc.Nolvus.NexusApi.Responses;
 
-namespace Nolvus_Dashboard_V2.API_manager
+class Program
 {
-    
+    static async Task Main()
+    {
+        var socket = new ClientWebSocket();
+        await socket.ConnectAsync(new Uri("wss://sso.nexusmods.com"), CancellationToken.None);
+
+        // Checking if the user is signed in on nexusmods.com
+        if (true)
+        {
+            bool uuid = true; // Assuming you want to assign 'true' to uuid
+            var uuidValue = sessionStorage.GetItem("uuid");
+            var tokenValue = sessionStorage.GetItem("connection_token");
+
+            // Do something with uuidValue and tokenValue
+        }
+        else if (false)
+        {
+            var uuid = Guid.NewGuid().ToString();
+            string token = null;
+            sessionStorage.SetItem("uuid", uuid);
+
+            var data = new
+            {
+                id = uuid,
+                token = token,
+                protocol = 2
+            };
+
+            // Do something with 'data'
+        }
+    }
 }
 
+public static class sessionStorage
+{
+    public static string GetItem(string key)
+    {
+        // Implement your sessionStorage logic for retrieving an item by key
+        throw new NotImplementedException();
+    }
+
+    public static void SetItem(string key, string value)
+    {
+        // Implement your sessionStorage logic for setting an item
+        throw new NotImplementedException();
+    }
+}
